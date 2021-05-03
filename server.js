e703ab57c37d6e4d5d8e1749b7189b5ae48ec06c
unknown <nick_clymer2018@yahoo.com>
2021-05-02 18:50:09 -0600

    formatting cleanup

--- server.js
+++ server.js
@@ -7,24 +7,23 @@ const primeData = path.join(__dirname, "/public");
 
 app.use(express.json());
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // paths //
-app.get("*", function(req, res) {
-    res.sendFile(path.join(primeData, "index.html"));
-});
-
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(primeData, "notes.html"));
 });
 
-app.get("/api/notes", function(req, res) {
+app.get("/api/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
-// writes user entries to db.json
+app.get("*", function (req, res) {
+    res.sendFile(path.join(primeData, "index.html"));
+});
 
-app.post("/api/notes", function(req, res) {
+// writes user entries to db.json
+app.post("/api/notes", function (req, res) {
     let saveData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let newData = req.body;
     let uniqueID = (saveData.length).toString();
@@ -34,9 +33,9 @@ app.post("/api/notes", function(req, res) {
     fs.writeFileSync("./db/db.json", JSON.stringify(saveData));
     console.log("Note saved to db.json. Content: ", newData);
     res.json(saveData);
-})
+});
 
 // server init //
-app.listen(PORT, function() {
-    console.log(`The server has started and is now listening on port ${PORT}. Enter localhost:8080 into your URL bar to use the application.`);
-})
+app.listen(PORT, function () {
+    console.log(`The server has started and is now listening on port ${PORT}. Enter "localhost:8080" into your URL bar to use the application.`);
+});
